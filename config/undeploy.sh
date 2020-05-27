ae317621cc721aae6509b0b4d90bbd85254d70e7
schinche <suyog_chinche_tp@bmc.com>
2020-05-27 13:41:25 +0530

    updated undeploy files

--- config/undeploy.sh
+++ config/undeploy.sh
@@ -4,8 +4,5 @@
 application_name=$1
 [[ $application_name == '' ]] && application_name=ccoms;
 
-helm ls | grep $1 | awk '{print $1}' | xargs helm del --purge
-
-
-
+helm ls  | grep config | awk '{print $1}' | xargs helm del --purge
 
