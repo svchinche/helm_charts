85c7e881b40fb2812ac9987044a0a887304d4202
schinche <suyog_chinche_tp@bmc.com>
2020-05-27 12:28:55 +0530

    updated some values

--- mongo/deploy.sh
+++ mongo/deploy.sh
@@ -2,6 +2,8 @@
 
 application_name=$1
 
+[[ $application_name == '' ]] && application_name=ccoms;
+
 ## Create deployment
 helm install --debug --namespace=$application_name -f ../common/values.yaml  ../mongo
 
--- mongo/undeploy.sh
+++ mongo/undeploy.sh
@@ -3,6 +3,8 @@
 
 application_name=$1
 
+[[ $application_name == '' ]] && application_name=ccoms;
+
 helm ls | grep $1 | awk '{print $1}' | xargs helm del --purge
 SHARED_DIR="/u02/pvs"
 
