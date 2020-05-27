44d6e12aec898b1130e50d75156b68e0d5554d51
schinche <suyog_chinche_tp@bmc.com>
2020-05-27 13:27:05 +0530

    Updated file

--- config/deploy.sh
+++ config/deploy.sh
@@ -6,16 +6,3 @@ application_name=$1
 ## Create deployment
 helm install --debug --namespace=$application_name -f ../common/values.yaml  ../config
 
-sh get_data_from_json.sh
-
-## Create deployment
-kubectl get pod -n $application_name | grep -i running
-until [[ $? -eq 0 ]] ;
-do
-  echo "Retrying";
-  kubectl get pod -n $application_name | grep -i running
-  sleep 10;
-done
-
-kubectl exec -i -n $application_name  mongo-0 -- mongo < create_user.json
-
