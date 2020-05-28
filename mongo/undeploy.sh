1653f42a9dd9f775dd65a672eb97a8ea3e689bb2
schinche <suyog_chinche_tp@bmc.com>
2020-05-28 16:58:12 +0530

    Updated cleanup data

--- mongo/undeploy.sh
+++ mongo/undeploy.sh
@@ -2,8 +2,9 @@
 
 
 application_name=$1
-[[ $application_name == '' ]] && application_name=ccoms;
+SHARED_DIR="/u02/pvs"
 
+[[ $application_name == '' ]] && application_name=ccoms;
 
 pvs_var=(pv-nfs-pv0 pv-nfs-pv1 pv-nfs-pv2 pv-nfs-pv3 pv-nfs-pv4)
 
@@ -17,8 +18,9 @@ done
 
 helm ls  | grep mongo | awk '{print $1}' | xargs helm del --purge
 
-
-
+## Cleanup mongo data on nfs persistent volume 
+[[ ! -d $SHARED_DIR ]] && mkdir -p /u02/pvs
+[[ ! -d $SHARED_DIR/pv0 ]] && ( mkdir -p $SHARED_DIR/{pv0,pv1,pv2,pv3,pv4} && chmod -R 777 $SHARED_DIR/ )
 
 
 
