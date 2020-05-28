3b33a1cd3d802099026fda6b1b0b3d0671088310
schinche <suyog_chinche_tp@bmc.com>
2020-05-28 16:27:00 +0530

    removed unnecessary code

--- mongo/undeploy.sh
+++ mongo/undeploy.sh
@@ -4,21 +4,20 @@
 application_name=$1
 [[ $application_name == '' ]] && application_name=ccoms;
 
+
+pvs=(pv-nfs-pv0 pv-nfs-pv1 pv-nfs-pv2 pv-nfs-pv3 pv-nfs-pv4 pv-nfs-pv5)
+
+# finalizer value is set to 'protect' which will block the deletion.
+# K8s keep this as protected since volume data may be required in rollback case.
+for pv in $pvs
+do
+   kubectl patch $pv  -p '{"metadata":{"finalizers": []}}' --type=merge
+done
+
 helm ls  | grep mongo | awk '{print $1}' | xargs helm del --purge
 
-SHARED_DIR="/u02/pvs"
 
-kubectl patch pv pv-nfs-pv0  -p '{"metadata":{"finalizers": []}}' --type=merge
-kubectl patch pv pv-nfs-pv1  -p '{"metadata":{"finalizers": []}}' --type=merge
-kubectl patch pv pv-nfs-pv2  -p '{"metadata":{"finalizers": []}}' --type=merge
-kubectl patch pv pv-nfs-pv3  -p '{"metadata":{"finalizers": []}}' --type=merge
-kubectl patch pv pv-nfs-pv4  -p '{"metadata":{"finalizers": []}}' --type=merge
 
-kubectl delete pv pv-nfs-pv0 pv-nfs-pv1 pv-nfs-pv2 pv-nfs-pv3 pv-nfs-pv4 
-rm -rf /u02/pvs
-
-[[ ! -d $SHARED_DIR ]] && mkdir -p /u02/pvs
-[[ ! -d $SHARED_DIR/pv0 ]] && ( mkdir -p $SHARED_DIR/{pv0,pv1,pv2,pv3,pv4} && chmod -R 777 $SHARED_DIR/ )
 
 
 
