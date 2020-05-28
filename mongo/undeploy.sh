e378e6c0cef9f74ccec6b9daee85feb953d31a6f
schinche <suyog_chinche_tp@bmc.com>
2020-05-28 16:52:51 +0530

    removed unnecessary nfs pv

--- mongo/undeploy.sh
+++ mongo/undeploy.sh
@@ -5,7 +5,7 @@ application_name=$1
 [[ $application_name == '' ]] && application_name=ccoms;
 
 
-pvs_var=(pv-nfs-pv0 pv-nfs-pv1 pv-nfs-pv2 pv-nfs-pv3 pv-nfs-pv4 pv-nfs-pv5)
+pvs_var=(pv-nfs-pv0 pv-nfs-pv1 pv-nfs-pv2 pv-nfs-pv3 pv-nfs-pv4)
 
 # finalizer value is set to 'protect' which will block the deletion.
 # K8s keep this as protected since volume data may be required in rollback case.
