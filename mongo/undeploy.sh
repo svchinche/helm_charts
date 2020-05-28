cdb67088e8b3eaa3aa4cf2771c827d5a1d4506ac
schinche <suyog_chinche_tp@bmc.com>
2020-05-28 17:22:43 +0530

    Updated data

--- mongo/undeploy.sh
+++ mongo/undeploy.sh
@@ -3,10 +3,12 @@
 
 application_name=$1
 SHARED_DIR="/u02/pvs"
+pvs_var=(pv-nfs-pv0 pv-nfs-pv1 pv-nfs-pv2 pv-nfs-pv3 pv-nfs-pv4)
 
 [[ $application_name == '' ]] && application_name=ccoms;
 
-pvs_var=(pv-nfs-pv0 pv-nfs-pv1 pv-nfs-pv2 pv-nfs-pv3 pv-nfs-pv4)
+## deleting all resources
+helm ls  | grep mongo | awk '{print $1}' | xargs helm del --purge
 
 # finalizer value is set to 'protect' which will block the deletion.
 # K8s keep this as protected since volume data may be required in rollback case.
@@ -14,9 +16,9 @@ pvs_var=(pv-nfs-pv0 pv-nfs-pv1 pv-nfs-pv2 pv-nfs-pv3 pv-nfs-pv4)
 for pv in ${pvs_var[@]}
 do
    kubectl patch pv $pv -p '{"metadata":{"finalizers": []}}' --type=merge
+   kubectl delete pv $pv
 done
 
-helm ls  | grep mongo | awk '{print $1}' | xargs helm del --purge
 
 ## Cleanup mongo data on nfs persistent volume
 [[ ! -d $SHARED_DIR ]] && rm -rf $SHARED_DIR
