9152f56575c63e6d64c402966b4da2a4e7366177
schinche <suyog_chinche_tp@bmc.com>
2020-05-28 16:35:45 +0530

    added pv missiong option

--- mongo/undeploy.sh
+++ mongo/undeploy.sh
@@ -9,9 +9,10 @@ pvs=(pv-nfs-pv0 pv-nfs-pv1 pv-nfs-pv2 pv-nfs-pv3 pv-nfs-pv4 pv-nfs-pv5)
 
 # finalizer value is set to 'protect' which will block the deletion.
 # K8s keep this as protected since volume data may be required in rollback case.
+
 for pv in $pvs
 do
-   kubectl patch $pv  -p '{"metadata":{"finalizers": []}}' --type=merge
+   kubectl patch pv $pv -p '{"metadata":{"finalizers": []}}' --type=merge
 done
 
 helm ls  | grep mongo | awk '{print $1}' | xargs helm del --purge
