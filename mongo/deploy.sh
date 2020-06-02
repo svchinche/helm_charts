640f1aec2588407ceca6612556e1026cdd69f312
schinche <suyog_chinche_tp@bmc.com>
2020-06-02 18:51:58 +0530

    Namespace support for helm3

--- mongo/deploy.sh
+++ mongo/deploy.sh
@@ -25,6 +25,9 @@ done
 # include retry function
 . $SCRIPTPATH/../common/utilities.sh
 
+## Create namespace. as namespace creation at run time not supported in helm3
+kubectl create namespace $namespace
+
 ## Create deployment
 helm install --debug mongo --namespace=$namespace -f $SCRIPTPATH/../common/values.yaml  $SCRIPTPATH/../mongo
 
